44f2244c278946a927c8d4a47338f0d1dd7d6dfe
Wachirawit <wachirawit@iconext.co.th>
2019-01-02 08:54:28 +0700

    Create project.

---
+++ src/HttpRequesterWorkshop/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpRequesterWorkshop.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public OkObjectResult MethodOk()
+		{
+			return Ok(new
+			{
+				Message = "Response from 'MethodOk'"
+			});
+		}
+
+		public OkObjectResult MethodOk(string id)
+		{
+			return Ok(new
+			{
+				Message = $"Response from 'MethodOk' and id is {id}"
+			});
+		}
+
+		public BadRequestObjectResult MethodBadRequest()
+		{
+			return BadRequest(new
+			{
+				Message = "Response from 'MethodBadRequest'"
+			});
+		}
+	}
+}
