baa394c9a540a844f5c1adbd26f22f9912cb1eee
Wachirawit <wachirawit@iconext.co.th>
2019-01-02 09:34:52 +0700

    Update upload file workshop.

--- src/UploadWorkshop/Controllers/HomeController.cs
+++ src/UploadWorkshop/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using UploadWorkshop.ViewModels;
 
 namespace UploadWorkshop.Controllers
 {
@@ -6,7 +10,29 @@ namespace UploadWorkshop.Controllers
 	{
 		public IActionResult Index()
 		{
-			return View();
+			var model = new UploadViewModel();
+			return View(model);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Upload(UploadViewModel model)
+		{
+			var rootPath = hostingEnvironment.WebRootPath;
+			var filePath = Path.Combine(rootPath, model.File.FileName);
+
+			using (var fileStream = new FileStream(filePath, FileMode.Create))
+			{
+				await model.File.CopyToAsync(fileStream);
+			}
+
+			return RedirectToAction(nameof(Index));
+		}
+
+		private readonly IHostingEnvironment hostingEnvironment;
+
+		public HomeController(IHostingEnvironment hostingEnvironment)
+		{
+			this.hostingEnvironment = hostingEnvironment;
 		}
 	}
 }
---
+++ src/UploadWorkshop/ViewModels/UploadViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace UploadWorkshop.ViewModels
+{
+	public class UploadViewModel
+	{
+		public IFormFile File { get; set; }
+	}
+}
