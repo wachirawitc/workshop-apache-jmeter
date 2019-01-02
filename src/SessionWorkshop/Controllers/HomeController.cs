98082b0ea03c0c63ec2bfb7fe91f00acc13a4ecc
Wachirawit <wachirawit@iconext.co.th>
2019-01-02 13:40:07 +0700

    Update session workshop

---
+++ src/SessionWorkshop/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SessionWorkshop.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
